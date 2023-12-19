5628db0886aff63fbfa40506455cc5102aa051b7
Bram Vernimmen <vernimmenbram@gmail.com>
2023-12-19 16:52:13 +0100

    Added Base Animal, Lion and Zebra prefabs + Stats script
    Created a Base Animal prefab.
    Created 2 variants on this; Lion and Zebra.
    
    Created a BaseAnimalStats.
    These contain the stats that every animal will have.
    Thus this is put on the BaseAnimal prefab.

---
+++ HerdSimulation/Assets/Scripts/BaseAnimalStats.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAnimalStats : MonoBehaviour
+{
+    public int _maxHealth;
+    public int _health;
+    public float _hunger;
+    public float _hungerTick;
+    public float _thirst;
+    public float _thirstTick;
+
+    void Start()
+    {
+        _health = _maxHealth;
+    }
+
+    void Update()
+    {
+        _hunger = Mathf.Clamp(_hunger - _hungerTick * Time.deltaTime, 0.0f, 100000.0f);
+        if (_hunger <= 0)
+        {
+            Debug.Log("hungry");
+        }
+
+        _thirst = Mathf.Clamp(_thirst - _thirstTick * Time.deltaTime, 0.0f, 100000.0f);
+        if (_thirst <= 0)
+        {
+            Debug.Log("thirsty");
+        }
+    }
+}
